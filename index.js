e8380c6d5e7ffc6e631061bdefa01da4336faebb
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-12-24 12:31:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ app.use(express.static('public')); app.get('/api.blogs.add', async (req, res) =>
             .catch((err) => { res.send(404) })
     } catch (err) { res.send(404) }
 }); app.get('/post', async (req, res) => {
-    console.log(unescape(req.url.replace('/post?', '')))
+    console.log(req.url.replace('/post?', ''))
     try {
-        Blogs.findById(unescape(req.url.replace('/post?', '')))
+        Blogs.findById(req.url.replace('/post?', ''))
             .then((result) => {
                 fs.readFile(__dirname + '/public/posts/code.sample', { encoding: 'utf8' }, async (err, data) => {
                     await res.send(data.replaceAll('$@title', result.title).replace('$@date', result.date).replace('$@image', result.image).replace('$@body', result.body))
